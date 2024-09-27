79d72a7390aade42ed8d9e2e0d6e749a4f88ae84
강태양 <zxcv2987@naver.com>
2024-09-27 16:54:09 +0900

    init: netx.js 14, ts eslint, prettier, styled-components

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "동글",
+    description: "순천대학교 동아리",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+            <h1>동글</h1>
+        </>
+    );
+}
